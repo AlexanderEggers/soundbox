1158eacb6fac924c6712dd60505f959dc98aa960
Nikolai Reinke <nikolai.reinke@haw-hamburg.de>
2016-05-11 23:24:40 +0200

    audiostuff added

--- app/src/main/java/com/thm/sensors/activity/MasterActivity.java
+++ app/src/main/java/com/thm/sensors/activity/MasterActivity.java
@@ -12,15 +12,21 @@ import android.widget.Toolbar;
 
 import com.thm.sensors.R;
 import com.thm.sensors.Util;
+import com.thm.sensors.logic.AudioLogic;
 import com.thm.sensors.logic.BluetoothLogic;
 
+import org.puredata.android.io.PdAudio;
+
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.text.MessageFormat;
+
 
 public final class MasterActivity extends Activity {
 
     private BluetoothLogic mBluetoothLogic;
     private Handler mHandler;
+    private AudioLogic mAudioLogic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +58,21 @@ public final class MasterActivity extends Activity {
         ((TextView) findViewById(R.id.textView4)).setText("Heartbeat: n/a");
         ((TextView) findViewById(R.id.textView5)).setText("Acceleration: n/a");
         ((TextView) findViewById(R.id.textView6)).setText("Beacon-ID: n/a");
+
+        mAudioLogic = new AudioLogic(this);
+
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                Looper.prepare();
+                try{
+                    mAudioLogic.loadPDPatch();
+                    mAudioLogic.initPD();
+                } catch (IOException e){
+                    finish();
+                }                return null;
+            }
+        }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
 
     @Override
@@ -85,11 +106,17 @@ public final class MasterActivity extends Activity {
         wrapped = ByteBuffer.wrap(aValue);
         float value = wrapped.getFloat();
 
-        /*---Audio logic part could start here---*/
+
+
+
 
         switch (identifier) {
             case Util.PROXIMITY:
                 ((TextView) findViewById(R.id.textView3)).setText(MessageFormat.format("Proximity: {0}", value));
+
+                //AudioLogic handlen
+                mAudioLogic.processAudioProximity(value);
+
                 break;
             case Util.HEARTBEAT:
                 ((TextView) findViewById(R.id.textView4)).setText(MessageFormat.format("Heartbeat: {0}", value));
@@ -106,5 +133,17 @@ public final class MasterActivity extends Activity {
     protected void onStop() {
         super.onStop();
         mBluetoothLogic.close();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        PdAudio.startAudio(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        PdAudio.stopAudio();
     }
 }
--- app/src/main/java/com/thm/sensors/logic/AudioLogic.java
+++ app/src/main/java/com/thm/sensors/logic/AudioLogic.java
@@ -1,5 +1,50 @@
 package com.thm.sensors.logic;
 
+import android.app.Activity;
+
+import java.io.File;
+import java.io.IOException;
+import com.thm.sensors.R;
+
+import org.puredata.android.io.AudioParameters;
+import org.puredata.android.io.PdAudio;
+import org.puredata.android.utils.PdUiDispatcher;
+import org.puredata.core.PdBase;
+import org.puredata.core.utils.IoUtils;
+
+
+
 public final class AudioLogic {
-    //TODO: This class could be responsible to handle the audio stuff, like modifying audio sample.
+
+    private Activity mContext;
+    private PdUiDispatcher dispatcher;
+
+
+    public AudioLogic(Activity context) {
+        mContext = context;
+    }
+
+    public void processAudioProximity(float value) {
+        //send gyro value as well!!!
+        //map this value!!!
+        PdBase.sendFloat("toneHeight", 127.0f);
+
+    }
+
+
+    public void initPD() throws IOException {
+        int sampleRate = AudioParameters.suggestSampleRate();
+        PdAudio.initAudio(sampleRate, 0, 2, 8, true);
+        dispatcher = new PdUiDispatcher();
+        PdBase.setReceiver(dispatcher);
+    }
+
+    public void loadPDPatch() throws IOException{
+        File dir = mContext.getFilesDir();
+        IoUtils.extractZipResource(mContext.getResources().openRawResource(R.raw.simplepatch), dir, true);
+        File pdPatch = new File(dir, "simplepatch.pd");
+        PdBase.openPatch(pdPatch.getAbsolutePath());
+        PdAudio.startAudio(mContext);
+    }
+
 }
